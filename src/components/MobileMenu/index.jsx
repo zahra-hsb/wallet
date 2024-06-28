86b09925abe38d09781aba8c0394ea818c749cac
zahra <zahra20hb@gmail.com>
2024-06-28 14:49:46 -0700

    logout method created

--- src/components/MobileMenu/index.jsx
+++ src/components/MobileMenu/index.jsx
@@ -7,22 +7,31 @@ import Logout from '../../../public/icons/logout.png'
 import Link from 'next/link'
 import Image from 'next/image'
 import { IoMdClose } from "react-icons/io";
+import { useAccountEffect, useDisconnect } from 'wagmi'
+import { useRouter } from 'next/navigation'
 
 
 const MobileMenu = ({ close }) => {
+    const { disconnect } = useDisconnect()
+    const router = useRouter()
     const menuArray = [
         { name: 'Dashboard', url: '/dashboard', icon: DashboardIcon },
         { name: 'Deposit', url: '/deposit', icon: DepositIcon },
         { name: 'Withdraw', url: '/withdraw', icon: Withdraw },
         { name: 'Referral', url: 'referral', icon: Referral },
         { name: 'Support', url: '/support', icon: Support },
-        { name: 'Logout', url: '/logout', icon: Logout },
     ]
+
+    function handleLogout() {
+        disconnect()
+    }
+
+    
     return (
         <>
 
             <ul className='bg-[#000000b3] p-20 flex flex-col items-start justify-start gap-5 w-screen h-screen fixed top-0 left-0'>
-                <IoMdClose size={28} fill='red' onClick={close}/>
+                <IoMdClose size={28} fill='red' onClick={close} />
                 {menuArray?.map((item, index) => (
                     <>
                         <li key={index} className='flex gap-3'>
@@ -31,6 +40,10 @@ const MobileMenu = ({ close }) => {
                         </li>
                     </>
                 ))}
+                <li className='flex gap-3'>
+                    <Image width={25} src={Logout} alt='' />
+                    <button onClick={() => handleLogout()}>Logout</button>
+                </li>
             </ul>
         </>
     )
