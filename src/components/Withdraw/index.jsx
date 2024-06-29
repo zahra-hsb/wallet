8384e6b9174b0970f83afc72d397b69669862ccb
zahra <zahra20hb@gmail.com>
2024-06-29 08:00:19 -0700

    recent activity component created

--- src/components/Withdraw/index.jsx
+++ src/components/Withdraw/index.jsx
@@ -1,7 +1,18 @@
 import Image from "next/image"
 import Container from "../Container"
+import commision from '../../../public/icons/Data.png'
+import deposit from '../../../public/icons/Row.png'
+import withdraw from '../../../public/icons/Body.png'
 
 const Withdraw = () => {
+    const dataArray = [
+        { icon: commision, title: 'commision', date: '14 Mar, 2021', title1: 'n(Matic)', action: 'Amount', color: '#00FF7F' },
+        { icon: deposit, title: 'deposit', date: '14 Mar, 2021', title1: 'n(Matic)', action: 'Panel', color: '#FFF50A' },
+        { icon: withdraw, title: 'withdraw', date: '14 Mar, 2021', title1: 'n(Matic)', action: 'Panel', color: '#FD625E' },
+        { icon: withdraw, title: 'withdraw', date: '14 Mar, 2021', title1: 'n(Matic)', action: 'Panel', color: '#FD625E' },
+        { icon: commision, title: 'commision', date: '14 Mar, 2021', title1: 'n(Matic)', action: 'Amount', color: '#00FF7F' },
+        { icon: commision, title: '(Number_ID)', date: '14 Mar, 2021', title1: 'n(Matic)', action: 'Panel', color: '#00FF7F' },
+    ]
     return (
         <>
             <section className="py-20 bg-main">
@@ -14,43 +25,51 @@ const Withdraw = () => {
                     </div>
                 </Container>
                 <Container>
-                    <p>Submit a withdrawal request</p>
-                    <div className="flex flex-col w-full gap-1">
-                        <label>Amount</label>
-                        <input type="text" className="p-2 rounded" placeholder="Enter Amount" />
-                    </div>
-                    <div className="flex flex-col w-full gap-1">
-                        <label>Note</label>
-                        <input type="text" className="p-2 rounded" placeholder="Enter notes (Optimal)" />
-                    </div>
-                    <div className="flex flex-col w-full gap-1">
-                        <label>Password</label>
-                        <input type="text" className="p-2 rounded" placeholder="Enter your password" />
-                    </div>
-                    <div className="flex gap-1 justify-start w-full">
-                        <input type="checkbox" id="remember" className="rounded" />
-                        <label htmlFor="remember" className="text-[#00FF5E]">Remember this choice as default</label>
-                    </div>
-                    <button className="py-1 px-6 border rounded-full border-[#20FF44]">Submit</button>
+                    <form className="w-full flex flex-col gap-3 items-start">
+                        <p>Submit a withdrawal request</p>
+                        <div className="flex flex-col w-full gap-1">
+                            <label>Amount</label>
+                            <input type="text" className="p-2 rounded" placeholder="Enter Amount" />
+                        </div>
+                        <div className="flex flex-col w-full gap-1">
+                            <label>Note</label>
+                            <input type="text" className="p-2 rounded" placeholder="Enter notes (Optimal)" />
+                        </div>
+                        <div className="flex flex-col w-full gap-1">
+                            <label>Password</label>
+                            <input type="text" className="p-2 rounded" placeholder="Enter your password" />
+                        </div>
+                        <div className="flex gap-1 justify-start w-full">
+                            <input type="checkbox" id="remember" className="rounded" />
+                            <label htmlFor="remember" className="text-[#00FF5E]">Remember this choice as default</label>
+                        </div>
+                        <div className="w-full text-center">
+                            <button className="py-1 px-6 border rounded-full border-[#20FF44] text-center">Submit</button>
+                        </div>
+                    </form>
 
                 </Container>
-                
+
                 <div className='shadow-main text-white flex flex-col items-center gap-2 border border-[#00F0FF] rounded-2xl m-8 pt-3'>
                     <h3 className='font-bold text-lg'>Transactions</h3>
-                    <div className='w-full flex flex-col items-center border border-[#00F0FF] rounded-2xl p-3'>
-                        <div className="w-full flex justify-between">
-                            <div className="flex">
-                                <Image src={''} alt="" />
-                                <span>
-                                    <p>Commision</p>
-                                    <p>14 Mar, 2021</p>
-                                </span>
-                            </div>
-                            <span>
-                                <p>n(Matic)</p>
-                                <p>Amount</p>
-                            </span>
-                        </div>
+                    <div className='w-full flex flex-col  gap-5 items-center border border-[#00F0FF] rounded-2xl p-3'>
+                        {dataArray?.map((item, index) => (
+                            <>
+                                <div key={index} className="w-full flex justify-around">
+                                    <div className="flex gap-3 items-center">
+                                        <Image width={20} height={20} src={item.icon} alt="" />
+                                        <span>
+                                            <p className={item.title === 'commision' ? `text-[#00FF7F]` : item.title === 'deposit' ? 'text-[#FFF50A]' : item.title === 'withdraw' ? 'text-[#FD625E]' : 'text-[#00FF7F]'}>{item.title}</p>
+                                            <p>{item.date}</p>
+                                        </span>
+                                    </div>
+                                    <span>
+                                        <p className={`text-[${item.color}]`}>{item.title1}</p>
+                                        <p>{item.action}</p>
+                                    </span>
+                                </div>
+                            </>
+                        ))}
                     </div>
                 </div>
             </section>
