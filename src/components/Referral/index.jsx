ca7593ca6d56190eccea63d5873ae63e2d1fcb84
zahra <zahra20hb@gmail.com>
2024-07-06 13:00:12 -0700

    copy ref code created from database

--- src/components/Referral/index.jsx
+++ src/components/Referral/index.jsx
@@ -1,31 +1,14 @@
 'use client'
 import { useState } from 'react';
 import styles from '../../app/Home.module.css'
+import axios from 'axios';
+import { useAccount } from 'wagmi';
 
 const Referral = () => {
-    // const [referralCode, setReferralCode] = useState('')
-    const [isCopied, setCopy] = useState(false)
-    function createRefCode() {
-        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        let result = "";
-        for (let i = 0; i < 12; i++) {
-            result += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        const resultRefCode = 'https://regalchain.vercel.app/' + result
-        navigator.clipboard.writeText(resultRefCode);
-        // if (document.execCommand('copy', false , result)) {
-        setTimeout(() => {
-            setCopy(true)
-        }, 1000)
-        // }
-        // console.log(navigator.clipboard.writeText(result))
-        const isExistRefCode = getUsers()
-        
-        if(!isExistRefCode) {
-            resultRefCode
-        } 
-    }
 
+    const [isCopied, setCopy] = useState(false)
+    const [refCode, setRefCode] = useState('')
+    const { address } = useAccount()
 
     async function getUsers() {
         try {
@@ -35,26 +18,32 @@ const Referral = () => {
                 const errorData = await res.json();
                 throw new Error(`Failed to fetch users: ${errorData.message}`);
             }
-            const foundReferral = res.data?.find(item => item.address === address).referralCode
-            if (foundReferral === '') {
-                return false
-            } else {
-                return true
-            }
 
+            const result = res.data?.find(item => item.address === address).referralCode
+            setRefCode(result)
+            console.log('fg: ', result.toString());
+            return result; 
         } catch (err) {
             console.error("Error fetching users:", err);
             return null;
         }
     }
 
+    async function copyRefCode() {
+        const result = await getUsers(); 
+        if (result) {
+            console.log(result);
+            navigator.clipboard.writeText(result);
+            setCopy(true);
+        }
+    }
 
     return (
         <>
             <section className='pt-5'>
                 <div className='border shadow-main text-white border-[#00F0FF] rounded-3xl flex items-center flex-col p-5 gap-5'>
-                    <p className='font-bold text-xl '>Referral</p>
-                    <button onClick={createRefCode} className='border cursor-pointer border-[#00F0FF] py-1 px-7 rounded-3xl shadow-main'>Click to Copy!</button>
+                    <p className='font-bold text-xl'>Referral</p>
+                    <button onClick={copyRefCode} className='border cursor-pointer border-[#00F0FF] py-1 px-7 rounded-3xl shadow-main'>Click to Copy!</button>
                     {isCopied && <p className='text-[#00F0FF]'>Referral code copied!</p>}
                 </div>
             </section>
@@ -62,4 +51,4 @@ const Referral = () => {
     )
 }
 
-export default Referral
+export default Referral;
