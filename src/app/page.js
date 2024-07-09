c161a425f1f50901563e65498d79554672e54669
MmD <mmdj27634@gmail.com>
2024-07-10 01:52:31 +0330

    ref : minor improvements and error fixes

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,9 @@ import MainPage from "@/components/MainPage";
 import RootLayout from "./layout";
 import Login from "@/components/Login";
 
-
-
 export default function Home() {
-  
   return (
     <>
-      
       <MainPage />
     </>
   );
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,96 +1,107 @@
-'use client'
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-import { useAccount, useAccountEffect, useDisconnect } from "wagmi"
-import MobileNav from "../MobileNav"
-import BrainComponent from "../BrainComponent"
-import TicketFeed from "../TicketFeed"
-import Referral from "../Referral"
-import styles from '../../app/Home.module.css'
-import ActivityDashboard from "../ActivityDashboard"
-import axios from "axios"
+"use client";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect } from "react";
+import { useAccount, useAccountEffect, useDisconnect } from "wagmi";
+import MobileNav from "../MobileNav";
+import BrainComponent from "../BrainComponent";
+import TicketFeed from "../TicketFeed";
+import Referral from "../Referral";
+import styles from "../../app/Home.module.css";
+import ActivityDashboard from "../ActivityDashboard";
+import axios from "axios";
 
 const Dashboard = () => {
-    const router = useRouter()
-    // const { disconnect } = useDisconnect()
-    const { address, isConnecting, isDisconnected, isConnected } = useAccount()
+  const router = useRouter();
+  // const { disconnect } = useDisconnect()
+  const { address, isConnecting, isDisconnected, isConnected } = useAccount();
 
-
-
-    function createRefCode() {
-        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        let result = "";
-        for (let i = 0; i < 12; i++) {
-            result += chars.charAt(Math.floor(Math.random() * chars.length));
-        }
-        return result;
+  function createRefCode() {
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < 12; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
     }
+    return result;
+  }
 
+  const addUser = useCallback(
+    async (referral) => {
+      try {
+        await axios.post("/api/postUser", {
+          address: address,
+          referralCode: referral,
+          price: 0,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [address]
+  );
 
-    async function addUser(referral) {
-        try {
-            await axios.post('/api/postUser', { address: address, referralCode: referral, price: 0 })
-        } catch (error) {
-            console.log(error);
+  const getUsers = useCallback(
+    async (referral) => {
+      try {
+        const res = await axios.get("/api/getUsers");
+
+        let isExistUser;
+        let foundAddress;
+        console.log("getUsers:50 =>", res.data);
+
+        if (res.status !== 200) {
+          const errorData = await res.json();
+          throw new Error(`Failed to fetch users: ${errorData.message}`);
+        } else if (res.data != []) {
+          foundAddress =
+            res.data?.some((item) => item.address === address) ?? [];
+          console.log("getUsers:58 =>", foundAddress);
         }
-    }
 
-    async function getUsers(referral) {
-        try {
-            const res = await axios.get('/api/getUsers')
-            let isExistUser;
-            console.log(res.data);
-            if (res.status !== 200) {
-                const errorData = await res.json();
-                throw new Error(`Failed to fetch users: ${errorData.message}`);
-            }
-            else if (res.data != []) {
-                const foundAddress = res.data?.some(item => item.address === address)
-                console.log(foundAddress);
-            }
-            if (foundAddress) {
-                isExistUser = true
-                return;
-            } else {
-                // save user
-                isExistUser = false
-                addUser(referral)
-            }
-            
-            if (foundAddress != []) return;
-            // save user
-            addUser(referral)
-
-        } catch (err) {
-            console.error("Error fetching users:", err);
-            return null;
+        if (!!foundAddress?.length) {
+          isExistUser = true;
+          return;
+        } else {
+          // save user
+          isExistUser = false;
+          addUser(referral);
         }
-    }
 
-    async function saveUser() {
-        const referral = createRefCode()
-        const resultRef = 'https://aismart.liara.run/' + referral
-        getUsers(resultRef)
-    }
-    useEffect(() => {
-        saveUser()
-    }, [])
+        if (!!foundAddress.length) return;
+        // save user
+        addUser(referral);
+      } catch (err) {
+        console.error("getUsers:50 =>", "Error fetching users:", err);
+        return null;
+      }
+    },
+    [address]
+  );
 
-    useAccountEffect({
-        onDisconnect() {
-            router.push('/')
-        }
-    })
-    return (
-        <>
-            <MobileNav />
-            <section className={styles.section}>
-                <BrainComponent />
-                <TicketFeed />
-            </section>
-            {/* <button onClick={() => disconnect}>disconnect</button> */}
-        </>
-    )
-}
+  async function saveUser() {
+    const referral = createRefCode();
+    const resultRef = "https://aismart.liara.run/" + referral;
+    getUsers(resultRef);
+  }
+  useEffect(() => {
+    saveUser();
+  }, []);
 
-export default Dashboard
+  useAccountEffect({
+    onDisconnect() {
+      router.push("/");
+    },
+  });
+  return (
+    <>
+      <MobileNav />
+      <section className={styles.section}>
+        <BrainComponent />
+        <TicketFeed />
+      </section>
+      {/* <button onClick={() => disconnect}>disconnect</button> */}
+    </>
+  );
+};
+
+export default Dashboard;
