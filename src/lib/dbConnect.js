a5094d13bdff6ff34a6d8545cbf642c20e9232c7
MmD <mmdj27634@gmail.com>
2024-07-09 23:29:34 +0330

    ref : modify dbConnect function

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,29 +1,66 @@
-import mongoose, { connect } from "mongoose";
-import clientPromise from "./mongodb";
+import mongoose from "mongoose";
 
-const connection = {}
-// const uri = 'mongodb://localhost/wallet'
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
-const uri = process.env.MONGODB_URI
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 async function dbConnect() {
-    if (connection.isConnected) {
-        return
-    }
-    try {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-        // const db = await mongoose.connect(process.env.MONGODB_URI);
-        const db = await mongoose.connect(uri);
-        
-        connection.isConnected = db.connections[0].readyState
-    } catch (err) {
-        console.log('mongo error:', err);
-    }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
+export default dbConnect;
 
-export default dbConnect
+// import mongoose, { connect } from "mongoose";
+// import clientPromise from "./mongodb";
+
+// const connection = {}
+// // const uri = 'mongodb://localhost/wallet'
+
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+// }
+
+// const uri = process.env.MONGODB_URI
+
+// async function dbConnect() {
+//     if (connection.isConnected) {
+//         return
+//     }
+//     try {
+
+//         // const db = await mongoose.connect(process.env.MONGODB_URI);
+//         const db = await mongoose.connect(uri);
+
+//         connection.isConnected = db.connections[0].readyState
+//     } catch (err) {
+//         console.log('mongo error:', err);
+//     }
+// }
+
+// export default dbConnect
