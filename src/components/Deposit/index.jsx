3a45d23c33a60575a1b9f0094c135180d0d786fa
zahra <zahra20hb@gmail.com>
2024-07-10 16:08:47 -0700

    referral code section created

--- src/components/Deposit/index.jsx
+++ src/components/Deposit/index.jsx
@@ -57,6 +57,17 @@ const Deposit = () => {
                     </div>
                 </form>
             </Container>
+            <Container>
+                <form className="w-full px-2">
+                    <div className="flex flex-col w-full gap-2">
+                        <label>Referral Code</label>
+                        <input type="text" name='amount' className="p-2 rounded text-gray-800 outline-none" placeholder="Enter Referral Code" />
+                    </div>
+                    <div className="w-full text-center py-4">
+                        <button className="py-1 px-6 border rounded-full border-[#20FF44] text-center shadow-green">Submit</button>
+                    </div>
+                </form>
+            </Container>
         </>
     )
 }
