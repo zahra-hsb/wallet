380b3d6f5b1d27bb2bf8a754f3b2a03078f2146d
zahra <zahra20hb@gmail.com>
2024-07-20 13:44:24 -0700

    one section added to dashboard

--- src/components/BrainComponent/index.jsx
+++ src/components/BrainComponent/index.jsx
@@ -11,17 +11,23 @@ const BrainComponent = () => {
             <div className='relative text-[#bbb]'>
                 <Image src={Line} alt='' className={'w-1/2 -mt-[60%] translate-x-[14%]'} />
                 <p className='absolute bottom-[30%] left-[15%] text-sm w-[120px] '>
-                    AI Market
-                    Best AI platform
+                    Invest usdt and matic
                 </p>
             </div>
             {/* <Image src={Line1} alt='' className={styles.line1} /> */}
             <div className='relative text-[#bbb]'>
 
                 <Image src={Line1} alt='' className={'w-1/2 translate-x-[93%] -mt-[50%] relative'} />
-                <p className='absolute bottom-[30%] right-[13%] text-sm w-[120px] text-right '>
-                    Invest usdt and matic
-                    Get 1.5% daily profit
+                <p className='absolute bottom-[34%] right-[13%] text-sm w-[120px] text-right '>
+                    Get 1.5 daily profit
+                </p>
+            </div>
+            <div className='relative text-[#bbb]'>
+                <Image src={Line} alt='' className={'w-1/2 -mt-[50%] translate-x-[14%]'} />
+                <p className='absolute bottom-[27%] left-[17%] text-xs max-w-[150px] w-[120px] '>
+                    Ai Won't Replace Humans
+                    But Humans With Ai Will
+                    Replace Humans Without Ai
                 </p>
             </div>
         </>
