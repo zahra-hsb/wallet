052b47d7dbe51a95f44e39af199fd21dd3f5ae9b
zahra <zahra20hb@gmail.com>
2024-07-25 16:36:10 -0700

    get deposits endpoint created

---
+++ src/app/api/getDeposits/route.js
@@ -0,0 +1,47 @@
+import dbConnect from "@/lib/dbConnect";
+import UsersModel from "@/lib/models/UsersModel";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+
+
+
+export async function GET(req) {
+    try {
+
+        const address = req.nextUrl.searchParams.get("address");
+        if (!address) {
+            return NextResponse.json({ error: "address parameter is missing" });
+        }
+        await dbConnect();
+
+
+        const user = await UsersModel.findOne({ address: address });
+
+
+        if (!user) {
+            return { error: 'the user does not exists' };
+        }
+
+
+        const friendsWithLevel1 = user.friends.filter(friend => friend.level === '1').map(friend => friend.address);
+        const friendsWithLevel2 = user.friends.filter(friend => friend.level === '2').map(friend => friend.address);
+        const friendsWithLevel3 = user.friends.filter(friend => friend.level === '3').map(friend => friend.address);
+
+
+        const usersWithPrices1 = await UsersModel.find({
+            address: { $in: friendsWithLevel1 } 
+        }).select('price').exec();
+        const usersWithPrices2 = await UsersModel.find({
+            address: { $in: friendsWithLevel2 } 
+        }).select('price').exec();
+        const usersWithPrices3 = await UsersModel.find({
+            address: { $in: friendsWithLevel3 } 
+        }).select('price').exec();
+
+        const usersWithPrices = [usersWithPrices1, usersWithPrices2, usersWithPrices3]
+        return NextResponse.json(usersWithPrices);
+    } catch (err) {
+        return NextResponse.json({ error: err.message });
+    }
+}
+
--- src/components/InfoTable/index.jsx
+++ src/components/InfoTable/index.jsx
@@ -8,36 +8,55 @@ const InfoTable = () => {
     const [lvl1, setLvl1] = useState([])
     const [lvl2, setLvl2] = useState([])
     const [lvl3, setLvl3] = useState([])
+
+    const [deposit1, setDeposit1] = useState([])
+    const [deposit2, setDeposit2] = useState([])
+    const [deposit3, setDeposit3] = useState([])
     const { address } = useAccount()
 
     function getlvl(friends, level) {
         const lvl = friends.filter(item => item.level == level)
-        console.log('level: ', lvl);  
         return lvl
     }
-    // function getlvl2(friends) {
-    //     const lvl2 = friends.find(item => item.level === '2')
-    //     setLvl2(lvl2)
-    // }
-    // function getlvl3(friends) {
-    //     const lvl3 = friends.find(item => item.level === '3')
-    //     setLvl3(lvl3)
-    // }
+
     useEffect(() => {
         async function getFriends() {
             try {
                 const friends = await axios.get(`/api/getFriends?address=${encodeURIComponent(address)}`)
                 setFriends(friends.data)
-                console.log(friends.data);
+
                 setLvl1(getlvl(friends.data.friends, 1))
                 setLvl2(getlvl(friends.data.friends, 2))
                 setLvl3(getlvl(friends.data.friends, 3))
+                return friends.data.friends
             } catch (error) {
                 console.log(error);
             }
         }
 
-        getFriends()
+        const friendsArray = getFriends()
+        async function getDeposits(address) {
+            try {
+                const deposits = await axios.get(`/api/getDeposits?address=${encodeURIComponent(address)}`)
+                console.log(deposits.data);
+                let sum1 = 0
+                deposits.data[0].map(item => sum1 += item.price )
+                console.log('object: ', sum1);
+                setDeposit1(sum1)
+                let sum2 = 0
+                deposits.data[1].map(item => sum2 += item.price )
+                console.log('object: ', sum2);
+                setDeposit2(sum2)
+                let sum3 = 0
+                deposits.data[2].map(item => sum3 += item.price )
+                console.log('object: ', sum3);
+                setDeposit3(sum3)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getDeposits(address)
 
     }, [address])
     return (
@@ -54,18 +73,21 @@ const InfoTable = () => {
                 <tr className="border-y border-y-gray-700">
                     <th className="text-white py-5">Level 1</th>
                     <th className="text-white py-5">15%</th>
-                    <th>{!lvl1 ? '0' : lvl1?.length}</th>
+                    <th>{!lvl1 ? 'Loading...' : lvl1?.length}</th>
+                    <th>{deposit1 ? deposit1 : '0'}</th>
                 </tr>
                 <tr className="border-y border-y-gray-700">
                     <th className="text-white py-5">Level 2</th>
                     <th className="text-white py-5">10%</th>
-                    <th>{!lvl2 ? '0' : lvl2?.length}</th>
+                    <th>{!lvl2 ? 'Loading...' : lvl2?.length}</th>
+                    <th>{deposit2 ? deposit2 : '0'}</th>
 
                 </tr>
                 <tr className="border-y border-y-gray-700">
                     <th className="text-white py-5">Level 3</th>
                     <th className="text-white py-5">5%</th>
-                    <th>{!lvl3 ? '0' : lvl3?.length}</th>
+                    <th>{!lvl3 ? 'Loading...' : lvl3?.length}</th>
+                    <th>{deposit3 ? deposit3 : '0'}</th>
 
                 </tr>
             </table>
