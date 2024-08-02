f04d8aa584ad47931e5abe209bc2c3fcd7ab64b8
zahra <85888973+zahra-hsb@users.noreply.github.com>
2024-08-02 14:44:26 +0330

    Update route.js payout section commented

--- src/app/api/payout/route.js
+++ src/app/api/payout/route.js
@@ -14,7 +14,8 @@ export async function POST(req) {
     }
 
     // const apiSecretKey = (data.type === 'payment') ? 'sandbox' : 'S35UYY-T3E96V-A3VNKK-SUDT3H';
-    const apiSecretKey = (data.type === 'payment') ? 'N1CGY7-7963BT-MCCLX7-V3F74B' : 'S35UYY-T3E96V-A3VNKK-SUDT3H';
+    const apiSecretKey = (data.type === 'payment') && 'N1CGY7-7963BT-MCCLX7-V3F74B' 
+// : 'S35UYY-T3E96V-A3VNKK-SUDT3H';
     const hmacHeader = req.headers.get('hmac');
     const calculatedHmac = crypto
         .createHmac('sha512', apiSecretKey)
@@ -31,14 +32,15 @@ export async function POST(req) {
                 console.log(err);
             }
             // Process payment data here
-        } else if (data.type === 'payout') {
-            console.log('Received payout callback:', data);
+        } 
+// else if (data.type === 'payout') {
+           // console.log('Received payout callback:', data);
             // Process payout data here
-            try {
-                await axios.put('/api/putTransaction', data)
-            } catch (err) {
-                console.log(err);
-            }
+          //  try {
+            //    await axios.put('/api/putTransaction', data)
+          //  } catch (err) {
+          //      console.log(err);
+         //   }
         }
 
         return NextResponse.json({ message: 'OK' }, { status: 200 });
