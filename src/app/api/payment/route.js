d4e423cdd020723df0de730a569f5ffcf4d96e5a
zahra <zahra20hb@gmail.com>
2024-08-03 12:10:36 -0700

    nothing

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -12,7 +12,7 @@ export async function POST(req) {
         // return NextResponse.json({ amount: amount })
         const url = 'https://api.oxapay.com/merchants/request';
         const data = JSON.stringify({
-            merchant: 'N1CGY7-7963BT-MCCLX7-V3F74B',
+            merchant: process.env.PAYMENT_API_KEY,
             // merchant: 'sandbox',
             amount: amount,
             callbackUrl: 'https://aismart.liara.run/api/payout',
