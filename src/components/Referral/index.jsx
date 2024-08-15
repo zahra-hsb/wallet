e227588e767cae276c9d86f217bdc66c74e6f227
zahra <zahra20hb@gmail.com>
2024-08-15 13:19:29 -0700

    count of friends added

--- src/components/Referral/index.jsx
+++ src/components/Referral/index.jsx
@@ -1,114 +1,112 @@
 'use client'
-import { useEffect, useState } from 'react';
-import styles from '../../app/Home.module.css'
-import axios from 'axios';
-import { useAccount } from 'wagmi';
-import { createRefCode } from '@/lib/methods';
-import ReferralBox from '../ReferralBox';
+import { useEffect, useState } from 'react';  
+import styles from '../../app/Home.module.css'  
+import axios from 'axios';  
+import { useAccount } from 'wagmi';  
+import { createRefCode } from '@/lib/methods';  
+import ReferralBox from '../ReferralBox';  
 
-const Referral = () => {
+const Referral = () => {  
+    const [refCode, setRefCode] = useState('')  
+    const [err, setErr] = useState('')  
+    const { address } = useAccount()  
+    const [refBoxes, setRefBox] = useState([])  
+    const [friendCounts, setFriendCounts] = useState([]); // جدید: state برای نگهداری تعداد دوستان  
+    let line = 1  
 
-    const [refCode, setRefCode] = useState('')
-    const [err, setErr] = useState('')
-    const { address } = useAccount()
-    const [refBoxes, setRefBox] = useState([])
-    let line = 1
+    function select(e) {  
+        const text = e.target.value  
+        e.target.select()  
+    }  
 
+    async function getPrice() {  
+        try {  
+            const result = await axios.get(`/api/getPrice?address=${encodeURIComponent(address)}`)  
+            return result.data.price.price  
+        } catch (error) {  
+            console.log(error);  
+        }  
+    }  
 
+    async function getUsers() {  
+        try {  
+            const res = await axios.get('/api/getUsers');  
 
+            if (res.status !== 200) {  
+                const errorData = await res.json();  
+                throw new Error(`Failed to fetch users: ${errorData.message}`);  
+            }  
 
-    function select(e) {
-        const text = e.target.value
-        e.target.select()
-    }
+            const user = res.data?.find(item => item.address === address);  
+            const result = user.referralCode;  
+            const resultFriend = user.friends;  
 
-    async function getPrice() {
-        try {
-            const result = await axios.get(`/api/getPrice?address=${encodeURIComponent(address)}`)
-            return result.data.price.price
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    async function getUsers() {
-        try {
-            const res = await axios.get('/api/getUsers')
+            const counts = result.map(referral => {  
+                return { refCode: referral.refCode, count: resultFriend.filter(friend => friend.refCode === referral.refCode).length };  
+            });  
 
-            if (res.status !== 200) {
-                const errorData = await res.json();
-                throw new Error(`Failed to fetch users: ${errorData.message}`);
-            }
+            setFriendCounts(counts); 
+            setRefBox(result);  
+            return result;  
+        } catch (err) {  
+            console.error("Error fetching users:", err);  
+            return null;  
+        }  
+    }  
 
-            const result = res.data?.find(item => item.address === address).referralCode
-            console.log(result);
-            setRefBox(result)
-            return result;
-        } catch (err) {
-            console.error("Error fetching users:", err);
-            return null;
-        }
-    }
+    async function updateUser(resultRef, address, line) {  
+        try {  
+            await axios.put('/api/editRef', { address, resultRef, line })  
+        } catch (error) {  
+            console.log(error);  
+        }  
+    }  
 
-    async function updateUser(resultRef, address, line) {
-        try {
-            await axios.put('/api/editRef', { address, resultRef, line })
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    async function addRefBox() {  
+        const price = await getPrice();  
+        if (price !== 0) {  
+            let result = createRefCode();  
+            const resultRef = 'https://aismart.liara.run/' + result;  
+            setRefCode(resultRef)  
+            setRefBox(prevBoxes => [...prevBoxes, { refCode: resultRef }]);  
+            updateUser(resultRef, address, line);  
+        } else {  
+            setErr('please do deposit then get your referral code!')  
+            setTimeout(() => {  
+                setErr('');  
+            }, 5000);  
+        }  
+    }  
 
+    useEffect(() => {  
+        async function getReferal() {  
+            await getUsers()  
+        }  
+        getReferal();  
+    }, []);  
 
-    async function addRefBox() {
-        const price = await getPrice()
-        console.log(price);
-        if (price != 0) {
-            let result = createRefCode();
-            const resultRef = 'https://aismart.liara.run/' + result
-            setRefCode(resultRef)
-            setRefBox(prevBoxes => [...prevBoxes, { refCode: resultRef }]);
-            updateUser(resultRef, address, line)
-
-        } else {
-            setErr('please do deposit then get your referral code!')
-            setTimeout(() => {
-                setErr('')
-            }, 5000)
-        }
-    }
-    useEffect(() => {
-        async function getReferal() {
-            const result = await getUsers()
-            setRefCode(result)
-        }
-        getReferal()
-    }, [])
-    return (
-        <>
-            <section className='pt-5'>
-                {/* <ReferralBox index={-1} copyRefCode={copyRefCode} isCopied={isCopied} err={err} refCode={refCode} select={select} /> */}
-                {refBoxes.length != 0 ? refBoxes.map((code, index) => (
-                    <>
-                        <ReferralBox key={index} index={index} err={err} refCode={code.refCode} select={select} />
-                    </>
-                ))
-                    :
-                    <div className='text-white text-center py-5'>
-                        please click on the add button for create your referral code!
-                    </div>
-                }
-                {err &&
-                    <>
-                        <div className='text-red-500 text-center'>
-                            {err}
-                        </div>
-                    </>
-                }
-                <div className='flex items-center w-full justify-center py-2'>
-                    <button onClick={addRefBox} className='border border-[#00F0FF] shadow-main rounded-full text-white px-3 py-1'>+</button>
-                </div>
-            </section>
-        </>
-    )
-}
+    return (  
+        <>  
+            <section className='pt-5'>  
+                {refBoxes.length !== 0 ? refBoxes.map((code, index) => (  
+                    <div key={index}>  
+                        <ReferralBox index={index} err={err} refCode={code.refCode} select={select} />  
+                        <div className='text-gray-400 text-center'>  
+                            {friendCounts[index] ? friendCounts[index].count : 0} friends  
+                        </div>  
+                    </div>  
+                )) : (  
+                    <div className='text-white text-center py-5'>  
+                        please click on the add button for create your referral code!  
+                    </div>  
+                )}  
+                {err && <div className='text-red-500 text-center'>{err}</div>}  
+                <div className='flex items-center w-full justify-center py-2'>  
+                    <button onClick={addRefBox} className='border border-[#00F0FF] shadow-main rounded-full text-white px-3 py-1'>+</button>  
+                </div>  
+            </section>  
+        </>  
+    )  
+}  
 
 export default Referral;
