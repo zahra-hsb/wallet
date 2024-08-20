9aa794d32766a7d22bae9c0a575f28496580e781
zahra <zahra20hb@gmail.com>
2024-08-20 05:50:23 -0700

    edit friends bug fixed

--- src/app/api/editFriends/route.js
+++ src/app/api/editFriends/route.js
@@ -7,7 +7,7 @@ export async function PUT(req) {
   try {
 
     const { data, upAddress, referralCode } = await req.json();
-    console.log(data, upAddress.address, referralCode);
+    console.log(data, upAddress, referralCode);
 
     await dbConnect();
     if (upAddress === null) {
@@ -22,50 +22,51 @@ export async function PUT(req) {
     const existingUser = await UsersModel.find({ address: data[0].address }, {})
 
     if (existingFriend.length === 0 && existingUser.length === 0) {
-    // 3 lvl 1 0x22
-    const updatedDoc = await UsersModel.findOneAndUpdate(
-      { address: upAddress.address },
-      { $push: { friends: { address: data[0].address, level: '1', line, refCode: referralCode } } }
-    );
+      // 3 lvl 1 0x22
+      const updatedDoc = await UsersModel.findOneAndUpdate(
+        { address: upAddress.address },
+        { $push: { friends: { address: data[0].address, level: '1', line, refCode: referralCode } } }
+      );
 
-    if (!updatedDoc) {
-      throw new Error('Document update failed.');
-    }
-    // 0x22
-    console.log('updatedDoc: ', updatedDoc);
-    // 2 lvl 2 0x59 !!!! 0x22
-    let user = await UsersModel.findOneAndUpdate(
-      { 'friends': { 'address': updatedDoc.address, 'level': '1' } },
-      { $push: { friends: { address: data[0].address, level: '2', line, refCode: referralCode } } }
-    )
+      if (!updatedDoc) {
+        throw new Error('Document update failed.');
+      }
+      // 0x22
+      console.log('updatedDoc: ', updatedDoc.address);
+      // 2 lvl 2 0x59 !!!! 0x22
 
-
-    console.log('found user: ', user); // 0x59
-
-    if (user) {
-      console.log('found user: ', user);
-
-      // 1 lvl 3
-      let user2 = await UsersModel.findOneAndUpdate(
-        { 'friends': { 'address': user.address, 'level': '1' } },
-        { $push: { friends: { address: data[0].address, level: '3', line, refCode: referralCode } } }
+      let user = await UsersModel.findOneAndUpdate(
+        { 'friends': { '$elemMatch': { 'address': updatedDoc.address, 'level': '1' } } },
+        { $push: { friends: { address: data[0].address, level: '2', line, refCode: referralCode } } }
       )
 
-      console.log(user.address);
 
-      console.log('user2: ', user2);
-    }
+      console.log('found user2: ', user); // 0x59
 
-    if (!updatedDoc) {
-      throw new Error('Document update failed.');
-    }
-    revalidatePath('/referral', 'page')
+      if (user) {
+        console.log('found user3: ', user);
 
-    return NextResponse.json({ updatedDoc });
+        // 1 lvl 3
+        let user2 = await UsersModel.findOneAndUpdate(
+          { 'friends': { '$elemMatch': { 'address': user.address, 'level': '1' } } },
+          { $push: { friends: { address: data[0].address, level: '3', line, refCode: referralCode } } }
+        )
+
+        console.log(user.address);
+
+        console.log('user2: ', user2);
+      }
+
+      if (!updatedDoc) {
+        throw new Error('Document update failed.');
+      }
+      revalidatePath('/referral', 'page')
+
+      return NextResponse.json({ updatedDoc });
 
     } else {
-    console.log('the friend exists in database');
-    return NextResponse.json({ message: 'Friend already exists in database' });
+      console.log('the friend exists in database');
+      return NextResponse.json({ message: 'Friend already exists in database' });
 
     }
   } catch (err) {
